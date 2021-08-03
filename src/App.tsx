5ca23d4de0f16229ce9f8563019e98e55e3ce808
ml1234256 <1064575122@qq.com>
2021-08-03 17:36:45 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>中国互联网络发展状况统计</header>
+      <main>
+        main
+        <section className="chart-1"></section>
+        <section className="chart-2"></section>
+        <section className="chart-3"></section>
+        <section className="chart-4"></section>
+        <section className="chart-5"></section>git
+        <section className="chart-6"></section>
+        <section className="chart-7"></section>
+        <section className="chart-8"></section>
+      </main>
+      <footer>
+        footer
+      </footer>
     </div>
   );
 }
