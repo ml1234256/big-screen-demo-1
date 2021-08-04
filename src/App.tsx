92bc89fb495de8cd3f182100d06e06d3be306823
ml1234256 <1064575122@qq.com>
2021-08-04 09:42:31 +0800

    优化文件结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { Route, HashRouter, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header>中国互联网络发展状况统计</header>
-      <main>
-        main
-        <section className="chart-1"></section>
-        <section className="chart-2"></section>
-        <section className="chart-3"></section>
-        <section className="chart-4"></section>
-        <section className="chart-5"></section>git
-        <section className="chart-6"></section>
-        <section className="chart-7"></section>
-        <section className="chart-8"></section>
-      </main>
-      <footer>
-        footer
-      </footer>
+      <HashRouter>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './home.scss';
+
+
+export const Home = () => {
+    return (
+        <div className="home">
+            <header>中国互联网络发展状况统计</header>
+            <main> 
+                <section className="section-1">1</section>
+                <section className="section-2">2</section>
+                <section className="section-3">3</section>
+            </main>
+            <footer>
+                footer
+            </footer>
+        </div>
+    );
+}
