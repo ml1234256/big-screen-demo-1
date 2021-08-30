7ab23deaa5e59918ccddaf6a465ff48b66aa0d84
ml1234256 <1064575122@qq.com>
2021-08-31 07:46:07 +0800

    完成表格布局

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,21 @@ export const Home = () => {
         <div className="home">
             <header><h1>中国互联网络发展状况统计</h1></header>
             <main> 
-                <section className="section-1">1</section>
-                <section className="section-2">2</section>
-                <section className="section-3">3</section>
+                <section className="section-1">
+                    <div className="chart chart-11"></div>
+                    <div className="chart chart-12"></div>
+                    <div className="chart chart-13"></div>
+                </section>
+                <section className="section-2">
+                    <div className="chart-21"></div>
+                    <div className="chart-22"></div>
+                    <div className="chart-23"></div>
+                </section>
+                <section className="section-3">
+                    <div className="chart chart-31"></div>
+                    <div className="chart chart-32"></div>
+                    <div className="chart chart-33"></div>
+                </section>
             </main>
             <footer>
                 footer
