2e147cfbd5999022e06c3bde8afd8dd1fc43cd4b
ml1234256 <1064575122@qq.com>
2021-09-02 17:28:02 +0800

    添加app分类饼图

--- src/components/chartApp.tsx
+++ src/components/chartApp.tsx
@@ -9,10 +9,32 @@ const ChartWrapper = styled.div`
 
 
 export const ChartApp:React.FC = () => {
-        const chartDiv = useRef<HTMLDivElement>(null);
+    const chartDiv1 = useRef<HTMLDivElement>(null);
+    const chartDiv2 = useRef<HTMLDivElement>(null);
         useEffect(() => {
-            const myChart = echarts.init((chartDiv as any).current);
-            let options = {
+            const myChart1 = echarts.init((chartDiv1 as any).current);
+            const myChart2 = echarts.init((chartDiv2 as any).current);
+            let options1 = {
+                title: {
+                    text: 'App分类占比'
+                },
+                xAxis: {show: false},
+                yAxis: {show: false},
+                legend: {show: false},
+                series: [{
+                    type: 'pie',
+                    radius: ['35%', '50%'],
+                    avoidLabelOverlap: false, 
+                    data: [
+                        { value: 25.7, name: "游戏" },
+                        { value: 14.6, name: "日常工具" },
+                        { value: 9.9, name: "电子商务" },
+                        { value: 9.0, name: "生活服务" },
+                        { value:40.8, name: "其他" }
+                    ]
+                }]
+            };
+            let options2 = {
             title: {
                 text: 'App 在架数量'
             },
@@ -37,12 +59,19 @@ export const ChartApp:React.FC = () => {
                 type: 'bar',  // color: 'red',
                 data: [402, 456, 367, 345]
             }]
-        };
-        options && myChart.setOption(options);
-        }, [])
+            };
+
+            options1 && myChart1.setOption(options1);
+            options2 && myChart2.setOption(options2);
+            }, [])
     return (
         <ChartWrapper>
-            <div ref={chartDiv}
+             <div ref={chartDiv1}
+                style={{
+                width: "100%",
+                height: "100%",
+            }}></div>
+            <div ref={chartDiv2}
                 style={{
                 width: "100%",
                 height: "100%",
