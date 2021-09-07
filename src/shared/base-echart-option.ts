cd11365e9381d26d17889c72585876d828cc9b00
ml1234256 <1064575122@qq.com>
2021-09-07 10:22:43 +0800

    修复baseEchartOption bug

--- src/shared/base-echart-option.ts
+++ src/shared/base-echart-option.ts
@@ -1,7 +1,7 @@
 import { px2rem } from "./px2rem";
 
 
-export const baseEchartsOption = {
+export const baseEchartOption = {
      textStyle: {
             fontSize: px2rem(12),
             color: '#79839E'
--- src/shared/create-echarts-option.ts
+++ src/shared/create-echarts-option.ts
@@ -1,20 +1,22 @@
-import { baseEchartsOption } from './base-echarts-option';
+import { baseEchartOption } from './base-echart-option';
 import { px2rem } from "./px2rem";
 
 export const createEchartOption = (option: any) => {
     const createOption = {
-        baseEchartsOption,
+        ...baseEchartOption,
         ...option,
     }
-    if (!(option?.xAxis?.axisLabel?.fontSize)) {
-        createOption.xAxis = createOption.xAxis || {};
-        createOption.xAxis.axisLabel = createOption.xAxis.axisLabel || {};
-        createOption.xAxis.axisLabel.fontSize = px2rem(12);
-    }
-    if (!(option?.yAxis?.axisLabel?.fontSize)) {
-        createOption.yAxis = createOption.yAxis || {};
-        createOption.yAxis.axisLabel = createOption.yAxis.axisLabel || {};
-        createOption.yAxis.axisLabel.fontSize = px2rem(12);
-    }
+    // if (!(option?.xAxis?.axisLabel?.fontSize)) {
+    //     createOption.xAxis = createOption.xAxis || {};
+    //     createOption.xAxis.axisLabel = createOption.xAxis.axisLabel || {};
+    //     createOption.xAxis.axisLabel.fontSize = px2rem(12);
+    // }
+    // if (!(option?.yAxis?.axisLabel?.fontSize)) {
+    //     createOption.yAxis = createOption.yAxis || {};
+    //     createOption.yAxis.axisLabel = createOption.yAxis.axisLabel || {};
+    //     createOption.yAxis.axisLabel.fontSize = px2rem(12);
+    // }
     return createOption
 }
+
+"修复baseEchartOption bug"
